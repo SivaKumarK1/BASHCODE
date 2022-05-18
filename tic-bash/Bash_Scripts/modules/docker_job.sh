f78713d98b423f96e1ad3af613547a1abd23e68f
SivaKumarK1 <90784290+SivaKumarK1@users.noreply.github.com>
2022-05-18 22:34:10 +0530

    Update docker_job.sh

--- tic-bash/Bash_Scripts/modules/docker_job.sh
+++ tic-bash/Bash_Scripts/modules/docker_job.sh
@@ -15,12 +15,12 @@ docker_job(){
         echo "============================================================================================="
         echo "Installing Docker"
         echo "============================================================================================="
-        sudo apt-get update
-        sudo apt-get install ca-certificates curl gnupg lsb-release
+        sudo apt-get update -y
+        sudo apt-get install ca-certificates curl gnupg lsb-release -y
         curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo apt-get update
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+        sudo apt-get update -y
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
         if [ "$?" -eq 0 ]
         then
             echo "============================================================================================="
