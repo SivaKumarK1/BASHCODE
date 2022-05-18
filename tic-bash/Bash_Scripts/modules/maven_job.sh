6f4617b8b6fc4153c65d650b990a5fe7175f179e
SivaKumarK1 <90784290+SivaKumarK1@users.noreply.github.com>
2022-05-18 19:07:40 +0530

    Update maven_job.sh

--- tic-bash/Bash_Scripts/modules/maven_job.sh
+++ tic-bash/Bash_Scripts/modules/maven_job.sh
@@ -1,13 +1,39 @@
 #!/bin/bash 
 # mvn installaton not working check in other instances
+proj_dir="../../../../MicroServiceRepo"
+
 mvn_job(){
     echo "Checking if Maven is installed or not"
-    echo `mvn -v`
-    if [ "$?" -eq  0 ]
+    mvn -v
+    if [ "$?" -eq  127 ]
+    then
+        echo "installing mvn"        
+        java -version
+        if [ "$?" -eq 127 ]
         then
-            echo "mvn installation found"
-            echo "Continuing Maven Build"
-            for pomLocation in `find ./* -name 'pom.xml' -type f -printf "%h\n" 2>/dev/null`
+                sudo apt install default-jdk -y
+                sudo apt install maven -y
+            if [ "$?" -eq 0 ]
+            then 
+                for pomLocation in `find ./* -name 'pom.xml' -type f -printf "%h\n" 2>/dev/null`
+                    do
+                        echo "Compiling with maven, inside $pomLocation"
+                        echo "============================================================================================="
+                        cd $pomLocation 
+                        mvn clean install 
+                        echo "============================================================================================="
+                        echo "============================================================================================="
+                        echo "Running Tests for your application"
+                        echo "for now empty"
+                        echo "============================================================================================="
+                        cd $proj_dir
+                    done
+            fi
+        fi    
+    else    
+        echo "mvn installation found"
+        echo "Continuing Maven Build"
+        for pomLocation in `find ./* -name 'pom.xml' -type f -printf "%h\n" 2>/dev/null`
             do
                 echo "Compiling with maven, inside $pomLocation"
                 echo "============================================================================================="
@@ -19,36 +45,24 @@ mvn_job(){
                 echo "for now empty"
                 echo "============================================================================================="
                 cd $proj_dir
-
-            done
-    else
-        echo "installing mvn"        
-        java -version
-        if [ "$?" -eq 127 ]
-            then
-                sudo apt install default-jdk -y
-                sudo apt install maven -y
-            if [ "$?" -eq 0 ]
-            then 
-                for pomLocation in `find ./* -name 'pom.xml' -type f -printf "%h\n" 2>/dev/null`
-                do
-                    echo "Compiling with maven, inside $pomLocation"
-                    echo "============================================================================================="
-                    cd $pomLocation
-                    mvn clean install 
-                    echo "============================================================================================="
-                    echo "============================================================================================="
-                    echo "Running Tests for your application"
-                    echo "for now empty"
-                    echo "============================================================================================="
-                    cd $proj_dir
-                done                
-            fi     
-        else
-            echo "failed to install maven"
-        fi
+            done                     
     fi
 
+#   curl -X 'POST' \
+#   'http://localhost:8082/tic/api/v1/pipeline/status/1/1/1' \
+#   -H 'accept: */*' \
+#   -H 'Content-Type: application/json' \
+#   -d '{
+#   "pipelineName": "pipeline", 
+#   "build": "true",
+#   "test": "true",
+#   "publish": "null",
+#   "infraStage": "null",
+#   "configStage": "null",
+#   "deploy": "null"
+# }'
+
+#accodring to us pipeline name is nekku voche cienv file lo projectname_pipeline
     # find ./* -name 'pom.xml' -type f -printf "%h\n" 2>/dev/null
     # the above command is used to find the pom.xml file inside the services 
     
