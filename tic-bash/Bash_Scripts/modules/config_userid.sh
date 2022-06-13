31247138d0e3561ddf9368a245a228ed3b303b64
SivaKumarK1 <sivakumar.k@comakeit.com>
2022-06-13 15:08:50 +0530

    changes

--- tic-bash/Bash_Scripts/modules/config_userid.sh
+++ tic-bash/Bash_Scripts/modules/config_userid.sh
@@ -17,6 +17,18 @@ sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(l
 sudo apt-get update -y && sudo apt-get install terraform -y
 echo "terraform installed"
 
+echo "########################################"
+echo "########################################"
+echo "#################### install docker #########################"
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get update -y
+echo "Docker installed"
+
 #install ansible
 echo "########################################"
 echo "########################################"
